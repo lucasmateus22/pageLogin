255ef3ef975f7fb970f022b2894a37bb0051144a
Lucas Mateus <104775719+lucasmateus22@users.noreply.github.com>
2022-11-21 20:22:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const color = document.querySelector('body')
+const color2 = document.querySelector('body')
 function red(){
-    if()
-}
+    if(
+        document.querySelector.classList.contains('blue')
+    ){
+        document.documentElement.classList.remove('blue');
+    }else{
+        document.documentElement.classList.add('blue');
+    }
+}document.documentElement.classList.add('blue');
+/**---------------------------------------------------------------------------- */
+
+const color3 = document.querySelector('body')
+
+const color1 = document.querySelector('body')
+function blue(){
+    if(
+        document.querySelector.classList.contains('red')
+    ){
+        document.documentElement.classList.remove('red');
+    }else{
+        document.documentElement.classList.add('red');
+    }
+}document.documentElement.classList.add('red');
+/**---------------------------------------------------------------------------- */
+
+
+function green(){
+    if(
+        document.querySelector.classList.contains('green')
+    ){
+        document.documentElement.classList.remove('green');
+    }else{
+        document.documentElement.classList.add('green');
+    }
+}document.documentElement.classList.add('green');
